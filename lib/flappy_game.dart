e5b763fd863c703e4bbd3c2b5f9c92eca46cfdec
Jérémy Riverain <jeremy@geekco.fr>
2022-12-01 21:01:23 +0100

    :rocket: configure flame

---
+++ lib/flappy_game.dart
@@ -0,0 +1,14 @@
+import 'dart:ui';
+import 'package:flame/game.dart';
+
+class FlappyGame extends Game {
+  @override
+  void render(Canvas canvas) {
+    // TODO: implement render
+  }
+
+  @override
+  void update(double dt) {
+    // TODO: implement update
+  }
+}
