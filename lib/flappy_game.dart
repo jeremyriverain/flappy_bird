8b39e36f0716842a8563fda42ef9e7d028da94cb
Jérémy Riverain <jeremy@geekco.fr>
2022-12-02 23:38:05 +0100

    :sparkles: add floor

--- lib/flappy_game.dart
+++ lib/flappy_game.dart
@@ -1,9 +1,36 @@
 import 'package:flame/game.dart';
+
 import 'package:flappy_bird/background.dart';
+import 'package:flappy_bird/floor.dart';
 
 class FlappyGame extends FlameGame {
+  List<Floor> floorComponents = [];
+
   @override
   Future<void> onLoad() async {
     add(Background());
+
+    _initFloorComponents();
+  }
+
+  @override
+  void update(double dt) {
+    final hiddenFloorComponents =
+        floorComponents.where((element) => element.hasDisappeared == true);
+    if (hiddenFloorComponents.length == 2) {
+      _initFloorComponents();
+    }
+    super.update(dt);
+  }
+
+  _initFloorComponents() {
+    removeAll(floorComponents);
+    floorComponents = [
+      Floor(initialLeftPosition: 0),
+      Floor(initialLeftPosition: size[0]),
+    ];
+    for (var floorComponent in floorComponents) {
+      add(floorComponent);
+    }
   }
 }
---
+++ lib/floor.dart
@@ -0,0 +1,27 @@
+import 'package:flame/components.dart';
+import 'package:flappy_bird/flappy_game.dart';
+
+class Floor extends SpriteComponent with HasGameRef<FlappyGame> {
+  final double initialLeftPosition;
+  bool hasDisappeared = false;
+
+  Floor({required this.initialLeftPosition});
+
+  @override
+  Future<void>? onLoad() async {
+    sprite = await Sprite.load('floor.png');
+    x = initialLeftPosition;
+    y = gameRef.size[1] * 7 / 8;
+    size = Vector2(gameRef.size[0], gameRef.size[1] / 8);
+    return super.onLoad();
+  }
+
+  @override
+  void update(double dt) {
+    position = Vector2(position.x - 130 * dt, gameRef.size[1] * 7 / 8);
+    if (position.x <= 0) {
+      hasDisappeared = true;
+    }
+    super.update(dt);
+  }
+}
